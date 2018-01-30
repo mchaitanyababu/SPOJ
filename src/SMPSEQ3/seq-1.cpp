3d23b7c4a3810efa6e6c07ace4fee2b2fcab25f2
Chaitanya M <chaitanyababu.m@gmail.com>
2018-01-30 15:32:30 +0530

    main
    main

---
+++ src/SMPSEQ3/seq-1.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+void READ(int *arr, unsigned short size)
+{
+    for (int i = 0; i < size; i++)
+        cin >> arr[i];
+}
+
+bool binsearch(int *list, int sz, int num)
+{
+    int s = 0, e = sz;
+    int mid = -1;
+
+    for (int i = 0; i < sz; ++i)
+    {
+        mid = (int)(s + e) / 2;
+
+        if (num == list[mid])
+        {
+            return true;
+        }
+        else if (num > list[mid])
+        {
+            s = mid;
+        }
+        else
+        {
+            e = mid;
+        }
+    }
+
+    return false;
+}
+
+int main()
+{
+    int nS = 0, nQ = 0;
+
+    cin >> nS;
+    int *S = new int[nS];
+    READ(S, nS);
+
+    cin >> nQ;
+    int *Q = new int[nQ];
+    READ(Q, nQ);
+
+    for (int i = 0; i < nS; ++i)
+    {
+        if (S[i] == Q[0])
+        {
+            continue;
+        }
+        else if (S[i] < Q[0])
+        {
+            cout << S[i] << " ";
+        }
+        else
+        {
+            binsearch(Q, nQ, S[i]) ? (cout << "") : (cout << S[i] << " ");
+        }
+    }
+
+    delete[] S;
+    delete[] Q;
+
+    return 0;
+}
